28d2fb2383f6a4707b13a8c32f47947abea3a047
Tom Alston <tom.alston@dearmansystems.com>
2018-03-05 15:46:49 -0700

    Added Detail Action Method

--- Scifi_Gallery/Controllers/SciFiBooksController.cs
+++ Scifi_Gallery/Controllers/SciFiBooksController.cs
@@ -6,10 +6,22 @@ using System.Web.Mvc;
 
 namespace Scifi_Gallery.Controllers
 {
+  // When first making the project I did a blank ASP.Net but clicked the MVC check
+  // box
+
   // Super important here. You have to add the "controller" suffix. It is 
   // more than a convention this helps MVC find the controller classes. MVC is a 
   // combination of views and controllers. 
+
+  // http://localhost:64488/SciFiBooks/Detail I can just put this in the Url and it
+  // it will show the return. 
   public class SciFiBooksController : Controller 
   {
+    // This is 
+    public string Detail()
+    {
+      return " Hello from SciFiBooks Controller "; 
+    }
+
   }
 }
