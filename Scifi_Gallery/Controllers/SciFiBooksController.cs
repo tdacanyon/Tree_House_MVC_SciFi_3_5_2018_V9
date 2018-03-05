fb880ccdb69cb6ce69db020259d31d11c84c1f81
Tom Alston <tom.alston@dearmansystems.com>
2018-03-05 15:30:52 -0700

    Added SciFiBooksController

---
+++ Scifi_Gallery/Controllers/SciFiBooksController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Scifi_Gallery.Controllers
+{
+  // Super important here. You have to add the "controller" suffix. It is 
+  // more than a convention this helps MVC find the controller classes. MVC is a 
+  // combination of views and controllers. 
+  public class SciFiBooksController : Controller 
+  {
+  }
+}
